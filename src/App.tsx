f3369b106674740b22ba92b471a32d4a59160f12
2kmkmkm <02dlrudals@gmail.com>
2025-08-31 22:48:35 +0900

    design: Implement SmallButton

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Button from "@components/common/Button";
+import SmallButton from "@components/signup/SmallButton";
+
 function App() {
   return (
     <>
+      <SmallButton label={"중복\n확인"} />
+      <SmallButton icon="bx:calendar" />
       <Button label="회원가입" />
-      <Button active={false} label="회원가입" />
+      <Button label="회원가입" active={false} />
+      <Button label="확인" modal={true} />
     </>
   );
 }
--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,19 @@
 export default function Button({
   active = true,
+  modal = false,
   label,
 }: {
   active?: boolean;
+  modal?: boolean;
   label: string;
 }) {
   return (
     <button
-      className={`w-full h-11 rounded-2xl flex justify-center items-center ${
+      className={`w-full body1 flex justify-center items-center ${
         active ? "bg-main" : "bg-gray"
-      }`}
+      } ${modal ? "h-9 max-w-40 rounded-xl" : "h-11 rounded-2xl"}`}
     >
-      <div className="body1 text-white">{label}</div>
+      <div className="text-white">{label}</div>
     </button>
   );
 }
