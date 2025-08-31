a00a8498e228461f33a1c3758dacf69547692a37
2kmkmkm <02dlrudals@gmail.com>
2025-08-31 23:00:38 +0900

    design: Implement Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import Button from "@components/common/Button";
-import SmallButton from "@components/signup/SmallButton";
-
+import Modal from "@components/common/Modal";
 function App() {
   return (
     <>
-      <SmallButton label={"중복\n확인"} />
-      <SmallButton icon="bx:calendar" />
-      <Button label="회원가입" />
-      <Button label="회원가입" active={false} />
-      <Button label="확인" modal={true} />
+      <Modal contents="안녕" />
     </>
   );
 }
