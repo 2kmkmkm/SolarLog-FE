a984ce2578c6f2083f70977d14a5915c385ec5f7
2kmkmkm <02dlrudals@gmail.com>
2025-08-31 20:35:46 +0900

    chore: Create tailwindCSS custom class

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <>Solarlog</>;
+  return (
+    <>
+      <div className="heading1">Solarlog</div>
+      <div className="heading2">Solarlog</div>
+      <div className="body1">Solarlog</div>
+      <div className="body2">Solarlog</div>
+      <div className="body2_bold">Solarlog</div>
+    </>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#FFA300",
+        sub: "#FF770F",
+        yellow: "#FFC326",
+        darkgray: "#636363",
+        gray: "#989898",
+        lightgray: "#C5C5C5",
+        blue: "#007DF2",
+        skyblue: "#3FB6FF",
+        green: "#72BC29",
+        lightgreen: "#F0F6CE",
+        red: "#FF3135",
+        pink: "#FF5C5E",
+        black: "#363636",
+        white: "#FAFAFA",
+      },
+      boxShadow: {
+        input: "0px 2px 10px 0px rgba(0,0,0,0.15)",
+        navigation: "0px -2px 20px 0px rgba(0,0,0,0.05)",
+        header: "0px 2px 20px 0px rgba(0,0,0,0.10)",
+        box: "0px 2px 50px 0px rgba(0,0,0,0.05)",
+      },
+    },
+  },
+  plugins: [],
+};
