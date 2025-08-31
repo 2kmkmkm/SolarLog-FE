71ac03a281b3d4cf1909f8afc0609dd5ef58415a
2kmkmkm <02dlrudals@gmail.com>
2025-08-31 21:09:06 +0900

    design: Implement LogoHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import LogoHeader from "@components/common/LogoHeader";
+
 function App() {
   return (
     <>
-      <div className="heading1">Solarlog</div>
-      <div className="heading2">Solarlog</div>
-      <div className="body1">Solarlog</div>
-      <div className="body2">Solarlog</div>
-      <div className="body2_bold">Solarlog</div>
+      <LogoHeader />
     </>
   );
 }
---
+++ src/components/common/LogoHeader.tsx
@@ -0,0 +1,7 @@
+export default function LogoHeader() {
+  return (
+    <div className="h-14 shadow-header px-6 flex items-center">
+      <img src="/Logo_img.svg" className="w-8 h-8" />
+    </div>
+  );
+}
