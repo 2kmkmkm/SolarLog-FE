c1351e94e532be582911d3f9e80fe3d836d19306
TurbineCrewGit <contact@turbinecrew.co.kr>
2025-09-05 10:51:36 +0900

    design: Implement LoginPage

--- src/components/login/LoginInput.tsx
+++ src/components/login/LoginInput.tsx
@@ -1,16 +1,13 @@
 import { Icon } from "@iconify/react";
 
-export default function LoginInput({
-  icon,
-  placeholder,
-}: {
+type LoginInputProps = {
   icon: string;
-  placeholder: string;
-}) {
+} & React.InputHTMLAttributes<HTMLInputElement>;
+export default function LoginInput({icon, ...props}: LoginInputProps) {
   return (
     <div className="shadow-input px-3 py-2 flex gap-1.5 border-[2.5px] border-main rounded-2xl">
       <Icon icon={icon} className="text-lightgray w-6 h-6" />
-      <input className="body1" placeholder={placeholder} />
+      <input className="body1 w-full bg-white" {...props} />
     </div>
   );
 }
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,3 +1,47 @@
+import Button from "@components/common/Button";
+import LoginInput from "@components/login/LoginInput";
+import { useState } from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <div className="h-screen flex flex-col justify-center items-center mx-12">
+      <div className="flex flex-col mt-14 gap-9">
+        <div className="flex flex-col justify-center items-center gap-1.5">
+          <img src="/Logo_img.svg" className="w-36 h-36" />
+          <img src="/Logo_word.svg" className="w-28" />
+        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col gap-6">
+          <div className="flex flex-col gap-3">
+            <LoginInput
+              icon="material-symbols:person-rounded"
+              placeholder="ID"
+              value={id}
+              id="id"
+              onChange={(e) => setId(e.target.value)}
+            />
+            <LoginInput
+              icon="mdi:password"
+              placeholder="password"
+              value={password}
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <Button label="로그인" type="submit" />
+            <span className="body2_bold text-lightgray">회원가입</span>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
